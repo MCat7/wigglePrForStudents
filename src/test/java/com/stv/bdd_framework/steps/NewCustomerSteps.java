86c846fc761ce442845a287839b2d688b51e0400
FantomasI <e.strelchuk1.21@gmail.com>
2022-10-24 11:04:33 +0300

    upd

--- src/test/java/com/stv/bdd_framework/steps/NewCustomerSteps.java
+++ src/test/java/com/stv/bdd_framework/steps/NewCustomerSteps.java
@@ -14,12 +14,13 @@ public class NewCustomerSteps extends BasicFactoryTest {
     MainFactoryPage mainFactoryPage = new MainFactoryPage();
     @Given("^myAccount page loaded$")
     public void myAccount_page_loaded() throws Throwable{
-        getDriver().manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         getDriver().get(START_URL_USA);
         mainFactoryPage.clickOnTrustButton();
         mainFactoryPage.clickOnAccountLink();
         boolean actualResult = mainFactoryPage.isAccountLinkDisplayed();
         Assert.assertEquals(actualResult, true, "Account link isn't visible");
         getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        getDriver().quit();
     }
 }
